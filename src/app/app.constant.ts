2176024a776bfd4fc1048ef32becb52f4c64113d
poojabastawadi7 <84792394+poojabastawadi7@users.noreply.github.com>
2022-08-22 15:38:24 +0530

    Update app.constant.ts

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -2,4 +2,16 @@ export const appConstant: any = {
     apiRoute: {
       products: 'products'
     },
+   forms: {
+    product: {
+      title: "title",
+      description: "description",
+      category: "category",
+      price: "price",
+      stock: "stock",
+      brand: "brand",
+      rating: "rating",
+    
+    }
+  }
   };
